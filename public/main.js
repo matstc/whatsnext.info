9a0777627bf1f3ed9e108db0c04039190e99e38f
Matt Tanguay-Carel <matthieutc@gmail.com>
2018-02-13 19:25:55 +0100

    defines the dependence on jquery properly and removes hack

--- public/main.js
+++ public/main.js
@@ -1,48 +1,45 @@
 requirejs.config({
-  paths: {jquery: 'jquery-2.1.0'}
+  paths: {jquery: 'jquery-2.1.0'},
+  deps: ["jquery", "require"],
+  callback: function(jquery, require){
+    require(['app', 'sammy', 'bootstrap'], function(app, sammy){
+      window.app = app;
+      jquery(document).ready(app.init);
+
+      sammy(function(){
+        this.get("/", function(){
+          this.redirect("#/about");
+        });
+
+        this.get("#/about", function(){
+          app.viewModel.activate(location.hash);
+          $(document.head.getElementsByTagName("title")).text("What's Next? — About");
+          jquery.getJSON('/contributors', app.viewModel.contributors);
+        });
+
+        this.get("#/:language", function(){
+          app.viewModel.repositories([]);
+          skippedLanguages = ['programming', 'rails', 'meteor'];
+          if ($.inArray(this.params['language'], skippedLanguages) == -1){
+            jquery.getJSON('/repositories/' + this.params['language'], app.viewModel.repositories);
+          }
+
+          app.viewModel.activate(location.hash);
+
+          if(typeof window !== "undefined" && app.viewModel.activeLanguage()){
+            var language = app.viewModel.activeLanguage();
+
+            jquery.getJSON('/resources/' + language.name.toLowerCase(), language.resources);
+
+            setTimeout(function(){
+              var toScroll = jquery("h1#section")[0].getBoundingClientRect().top + jquery(document.body).scrollTop() - 20;
+              jquery("html, body").animate({ scrollTop: toScroll + "px" });
+            }, 200);
+
+            $(document.head.getElementsByTagName("title")).text("What's Next? — " + language.name);
+          }
+        });
+      }).run();
+    });
+  }
 });
-
-define(['jquery', 'app', 'sammy', 'bootstrap'], function(jquery, app, sammy){
-  window.app = app;
-  jquery(document).ready(app.init);
-
-  if (typeof sammy === "undefined") // this happens once in a while (!)
-    location.reload()
-
-  sammy(function(){
-    this.get("/", function(){
-      this.redirect("#/about");
-    });
-
-    this.get("#/about", function(){
-      app.viewModel.activate(location.hash);
-      $(document.head.getElementsByTagName("title")).text("What's Next? — About");
-      jquery.getJSON('/contributors', app.viewModel.contributors);
-    });
-
-    this.get("#/:language", function(){
-      app.viewModel.repositories([]);
-      skippedLanguages = ['programming', 'rails', 'meteor'];
-      if ($.inArray(this.params['language'], skippedLanguages) == -1){
-        jquery.getJSON('/repositories/' + this.params['language'], app.viewModel.repositories);
-      }
-
-      app.viewModel.activate(location.hash);
-
-      if(typeof window !== "undefined" && app.viewModel.activeLanguage()){
-        var language = app.viewModel.activeLanguage();
-
-        jquery.getJSON('/resources/' + language.name.toLowerCase(), language.resources);
-
-        setTimeout(function(){
-          var toScroll = jquery("h1#section")[0].getBoundingClientRect().top + jquery(document.body).scrollTop() - 20;
-          jquery("html, body").animate({ scrollTop: toScroll + "px" });
-        }, 200);
-
-        $(document.head.getElementsByTagName("title")).text("What's Next? — " + language.name);
-      }
-    });
-  }).run();
-
-});
-
