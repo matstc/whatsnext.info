91c4ba6be820d06040d1eef11296f6818a2569b1
Martin Carel <martin.carel@gmail.com>
2018-02-14 09:54:30 -0500

    use Accept HTTP header as per github recommendation

--- lib/github.rb
+++ lib/github.rb
@@ -2,19 +2,18 @@ require 'open-uri'
 require 'json'
 
 class Github
-
-  def contributors repository
+  def contributors(repository)
     url = "https://api.github.com/repos/#{repository}/contributors"
     puts "Fetching url: #{url}"
-    JSON.parse open(url).read
+    JSON.parse open(url, 'Accept' => 'application/vnd.github.v3+json').read
   end
 
-  def most_starred language
+  def most_starred(language)
     url = "https://api.github.com/search/repositories?q=language:#{language}&sort=stars&order=desc"
     puts "Fetching url: #{url}"
     data = JSON.parse open(url).read
-    data['items'].map{|repo| repo.keep_if{|k,v| ['name', 'full_name', 'owner', 'stargazers_count', 'html_url', 'description'].include? k }}
+    data['items'].map do |repo|
+      repo.keep_if { |k, _v| %w[name full_name owner stargazers_count html_url description].include? k }
+    end
   end
-
 end
-
